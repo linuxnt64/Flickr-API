4af000c36e2cfa6d000a5be64e5c05aceac93e03
Staffan Simmons <staffan.simmons@gmail.com>
2022-02-01 20:05:05 +0100

    FlickrAPI fungerar inte längre trots att nyckeln ska vara aktiv

--- code.js
+++ code.js
@@ -3,10 +3,10 @@ const currentWidth = document.documentElement.clientWidth
 const photos = document.querySelector('#photoList');
 const searchTerm = "dogs";
 
-if (currentWidth < 400) { var sizeCode = "s" }
+if (currentWidth < 400)                         { var sizeCode = "s" }
 else if (currentWidth >= 400 && currentWidth < 550) { sizeCode = "t" }
 else if (currentWidth >= 550 && currentWidth < 800) { sizeCode = "q" }
-else { sizeCode = "m" };
+else                                                { sizeCode = "m" };
 
 getPictures(searchTerm);
 
@@ -21,9 +21,8 @@ const showPhotos = array => {
     array.forEach(photo => {
         const item = document.createElement('li');
         item.innerHTML = `<img src="https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_${sizeCode}.jpg" alt="${photo.title}">`;
-        if ( photo.id == 51467498709 || photo.id == 51467712075 || photo.id == 51466763306 ) {
-            photos.appendChild(item)
-        };
+//        if ( photo.id == 51467498709 || photo.id == 51467712075 || photo.id == 51466763306 ) {
+            photos.appendChild(item)        ;
     });
 }
 
